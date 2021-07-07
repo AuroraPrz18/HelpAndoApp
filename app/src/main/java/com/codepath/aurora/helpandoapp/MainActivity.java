aa9ec631831f3a4262548ce4f0027fa2c72104e9
Aurora Pérez <35460259+AuroraPrz18@users.noreply.github.com>
2021-07-07 13:08:10 -0500

    Initial commit

---
+++ app/src/main/java/com/codepath/aurora/helpandoapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codepath.aurora.helpandoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
