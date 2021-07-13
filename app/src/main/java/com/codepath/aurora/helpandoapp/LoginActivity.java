65c8a31118ab4c738c08f8ad71417218b58bbd31
Aurora Pérez <35460259+AuroraPrz18@users.noreply.github.com>
2021-07-13 16:10:23 -0500

    LogIn Activity created
    LogIn Activity UI completed, including content description that describes the element's purpose wich make the app more accessible. Moreover,  viewBinding was enabled. NOTE: It will need more work in the design.

---
+++ app/src/main/java/com/codepath/aurora/helpandoapp/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.codepath.aurora.helpandoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.codepath.aurora.helpandoapp.activities.SignUpActivity;
+import com.codepath.aurora.helpandoapp.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+    ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // Hide action bar in android
+        getSupportActionBar().hide();
+
+    }
+
+    public void onClickSignInButton(View view) {
+        // TODO: Logic of the program. Check if is a valid user, etc.
+        // Intent intent = new Intent(this, MainActivity.class);
+        //startActivity(intent);
+    }
+
+    public void onClickSignUp(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/codepath/aurora/helpandoapp/activities/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.codepath.aurora.helpandoapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.codepath.aurora.helpandoapp.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
